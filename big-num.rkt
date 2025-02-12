a8e4b1e1d5f70b45e618be92b3c1af6ec4216390
Prem Agarwal <prem.m.agarwal@gmail.com>
2025-02-12 14:08:27 -0800

    stuff

--- big-num.rkt
+++ big-num.rkt
@@ -1,8 +1,5 @@
 #lang racket
 
-;; The big-num data structure is essentially a list of 3 digit numbers.
-
-;; Exporting methods
 (provide big-add big-subtract big-multiply big-power-of pretty-print
          number->bignum string->bignum bignum? zero-or-one? one-block?)
 
@@ -27,7 +24,7 @@
     [1 #t]
     [_ #f]))
 
-;; Contract to insure a number is an integer in the range of 0-999.
+;; Contract to ensure a number is an integer in the range of 0-999.
 (define (one-block? n)
   (and (integer? n)
        (>= n 0)
@@ -36,112 +33,76 @@
 ;; Addition of two big-nums
 (define/contract (big-add x y)
   (-> bignum? bignum? bignum?)
-  (big-add1 x y 0)
-  )
+  (big-add1 x y 0))
 
 (define/contract (big-add1 x y co)
   (-> bignum? bignum? zero-or-one? bignum?)
   (cond
-    ;; If both lists are empty, the return value is either 0 or the caryover value.
-    [(and (= 0 (length x)) (= 0 (length y)))
-      (if (= co 0) '() (list co))]
-    [(= 0 (length x))  (big-add1 (list co) y 0)]
-    [(= 0 (length y))  (big-add1 x (list co) 0)]
+    [(and (null? x) (null? y)) (if (= co 0) '() (list co))]
+    [(null? x) (big-add1 (list co) y 0)]
+    [(null? y) (big-add1 x (list co) 0)]
     [else
-       ;;
-       ;; --- YOUR CODE HERE ---
-       ;;
-     (error "Not implemented")
-     ]
-    ))
+     (let* ([sum (+ (car x) (car y) co)]
+            [carry (if (>= sum MAX_BLOCK) 1 0)]
+            [digit (modulo sum MAX_BLOCK)]
+            [rest (big-add1 (cdr x) (cdr y) carry)])  ;; Recursive call with carry
+       (cons digit rest))]))  ;; Properly constructed list
 
 ;; Subtraction of two big-nums
 (define/contract (big-subtract x y)
   (-> bignum? bignum? bignum?)
   (let ([lst (big-subtract1 x y 0)])
-    (reverse (strip-leading-zeroes (reverse lst)))
-  ))
+    (reverse (strip-leading-zeroes (reverse lst)))))
 
 (define/contract (strip-leading-zeroes x)
   (-> bignum? bignum?)
   (cond
-    [(= 0 (length x)) '(0)]
+    [(null? x) '(0)]
     [(= 0 (car x)) (strip-leading-zeroes (cdr x))]
-    [else x]
-    ))
+    [else x]))
 
-;; NOTE: there are no negative numbers with this implementation,
-;; so 3 - 4 should throw an error.
+;; Subtraction helper function
 (define/contract (big-subtract1 x y borrow)
   (-> bignum? bignum? zero-or-one? bignum?)
-  ;;
-  ;; --- YOUR CODE HERE ---
-  ;;
-  (error "Not implemented"))
+  (cond
+    [(null? x) (error "Negative result not allowed")]  ;; Prevents negative numbers
+    [(null? y) (big-subtract1 x '(0) borrow)]  ;; If y is empty, assume zero
+    [else
+     (let* ([diff (- (car x) (car y) borrow)]
+            [new-borrow (if (< diff 0) 1 0)]
+            [adjusted-diff (if (< diff 0) (+ diff MAX_BLOCK) diff)]
+            [rest (big-subtract1 (cdr x) (cdr y) new-borrow)])  ;; Recursive call
+       (cons adjusted-diff rest))]))  ;; Properly constructed list
 
 ;; Returns true if two big-nums are equal
 (define/contract (big-eq x y)
   (-> bignum? bignum? boolean?)
-  ;;
-  ;; --- YOUR CODE HERE ---
-  ;;
-  (error "Not implemented"))
+  (equal? x y))
 
-;; Decrements a bignum
-(define/contract (big-dec x)
-  (-> bignum? bignum?)
-  (big-subtract x '(1))
-  )
-
-;; Multiplies two big-nums
+;; Multiplication of two big-nums
 (define/contract (big-multiply x y)
   (-> bignum? bignum? bignum?)
-  ;;
-  ;; --- YOUR CODE HERE ---
-  ;;
-  
-  ;; Follow the same approach that you learned in
-  ;; grade school for multiplying numbers, except
-  ;; that a "block" is 0-999, instead of 0-9.
-  ;; Consider creating a helper function that multiplies
-  ;; a big-number with a integer in the range of 0-999.
-  ;; Once you have that working, you can use it in your
-  ;; solution here.
-  (error "Not implemented"))
+  (foldl (lambda (digit acc)
+           (big-add (big-multiply1 x digit) acc))
+         '(0)
+         y))
+
+;; Multiply a bignum by a single digit (0-999)
+(define (big-multiply1 x digit)
+  (let loop ([x x] [carry 0])
+    (if (null? x)
+        (if (= carry 0) '() (list carry))
+        (let* ([prod (+ (* (car x) digit) carry)]
+               [carry (quotient prod MAX_BLOCK)]
+               [new-digit (modulo prod MAX_BLOCK)])
+          (cons new-digit (loop (cdr x) carry))))))
 
 ;; Raise x to the power of y
 (define/contract (big-power-of x y)
   (-> bignum? bignum? bignum?)
-  ;;
-  ;; --- YOUR CODE HERE ---
-  ;;
-  
-  ;; Solve this function in terms of big-multiply. 
-  (error "Not implemented"))
-
-;; Dispaly a big-num in an easy to read format
-(define (pretty-print x)
-  (let ([lst (reverse x)])
-    (string-append
-     (number->string (car lst))
-     (pretty-print1 (cdr lst))
-     )))
-
-(define (pretty-print1 x)
-  (cond
-    [(= 0 (length x))  ""]
-    [else
-     (string-append (pretty-print-block (car x)) (pretty-print1 (cdr x)))]
-    ))
-
-(define (pretty-print-block x)
-  (string-append
-   ","
-   (cond
-     [(< x 10) "00"]
-     [(< x 100) "0"]
-     [else ""])
-   (number->string x)))
+  (if (big-eq y '(0))
+      '(1)
+      (big-multiply x (big-power-of x (big-subtract y '(1))))))
 
 ;; Convert a number to a bignum
 (define/contract (number->bignum n)
@@ -159,3 +120,34 @@
   (let ([n (string->number s)])
     (number->bignum n)))
 
+;; Display a big-num in an easy-to-read format
+(define (pretty-print x)
+  (let ([lst (reverse x)])
+    (string-append
+     (number->string (car lst))
+     (pretty-print1 (cdr lst)))))
+
+(define (pretty-print1 x)
+  (cond
+    [(null? x)  ""]
+    [else
+     (string-append (pretty-print-block (car x)) (pretty-print1 (cdr x)))]))
+
+(define (pretty-print-block x)
+  (string-append
+   ","
+   (cond
+     [(< x 10) "00"]
+     [(< x 100) "0"]
+     [else ""])
+   (number->string x)))
+
+;; Example Usage:
+(display (pretty-print (big-add (number->bignum 999) (number->bignum 2))))
+(newline)
+(display (pretty-print (big-subtract (number->bignum 1000) (number->bignum 1))))
+(newline)
+(display (pretty-print (big-multiply (number->bignum 999) (number->bignum 999))))
+(newline)
+(display (pretty-print (big-power-of (number->bignum 2) (number->bignum 10))))
+(newline)
